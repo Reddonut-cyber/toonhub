3c70cafbabecc4f795fd6aa6788269f6939bd186
Soda <akhowsuk@gmail.com>
2026-03-07 15:38:49 +0700

    [+] Add some structure to routes file

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// public routes
+
 Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('home');
+
+// user zone
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 require __DIR__.'/settings.php';
+
+// admin zone
