fb8ec13787a731a085db5e1690619412e14d458d
Soda <akhowsuk@gmail.com>
2026-03-07 15:35:39 +0700

    [+] add welcome controler and use it

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', \App\Http\Controllers\WelcomeController::class)->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+require __DIR__.'/settings.php';
