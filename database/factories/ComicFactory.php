34febb65e085877a19d5ac0ecb23964fc3269552
Soda <akhowsuk@gmail.com>
2026-03-09 09:41:23 +0700

    [+]add ComicFactory for demo data

---
+++ database/factories/ComicFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comic>
+ */
+class ComicFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+        'name' => $this->faker->words(3, true), 
+        'summary' => $this->faker->paragraph(), 
+        'status' => $this->faker->randomElement(['Ongoing', 'Completed']),
+        'comic_web' => $this->faker->url(),
+        'category_id' => \App\Models\Category::factory(), 
+    ];
+    }
+}
