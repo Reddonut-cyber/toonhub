47afaf3131aa3bf55e48472276a46f1feed531a7
Soda <akhowsuk@gmail.com>
2026-03-12 09:53:24 +0700

    [feat] add IsAdminMiddleware to restrict comic creation routes to admin users

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ComicController; 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\IsAdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,20 +11,23 @@ Route::get('/', function () {
 
 // 2.Public Zone 
 Route::get('comics', [ComicController::class, 'index'])->name('comics.index');
-Route::get('comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
 
 // 3. User Zone 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
-    Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
+
+    Route::resource('comics', ComicController::class)
+        ->middleware(IsAdminMiddleware::class)
+        ->except(['index', 'show']);
 });
+
+Route::get('comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
 
 require __DIR__.'/auth.php';
 // require __DIR__.'/settings.php'; 
